d5e9b975fd4ce69533aeaffbcdbde1d7eb14bbe5
Biswajit Das <biswajitbangalore@gmail.com>
2017-01-17 19:48:17 +0530

    created metadat.rb

---
+++ test-cookbook/metadata.rb
@@ -0,0 +1,10 @@
+name 'test-cookbook'
+maintainer 'Biswajit Das'
+maintainer_email 'biswajitbangalore@gmail.com'
+license 'All rights reserved'
+description 'This cookbook is to test chef compliant'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version '1.0.0'
+%w(redhat suse).each do |os|
+  supports os
+end
