7a2386a73fbc158bc39a074f3a9db79a89ec1a9f
Biswajit Das <biswajitbangalore@gmail.com>
2017-01-17 19:46:02 +0530

    updated attribute file

---
+++ test-cookbook/attributes/default.rb
@@ -0,0 +1,5 @@
+# Cookbook: test-cookbook
+# Attribute file: default
+
+# default attributes for basic splunk install configuration
+node.default['test-cookbook']['attribute1'] = 'value from Attribute'
