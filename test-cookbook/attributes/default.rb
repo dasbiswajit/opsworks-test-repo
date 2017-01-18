e320e645623b09d5f415e7f84b5a565f80dc3bb7
Biswajit Das <biswajitbangalore@gmail.com>
2017-01-18 10:45:28 +0530

    Addd meaning full attribute.

--- test-cookbook/attributes/default.rb
+++ test-cookbook/attributes/default.rb
@@ -2,4 +2,4 @@
 # Attribute file: default
 
 # default attributes for basic splunk install configuration
-node.default['test-cookbook']['attribute1'] = 'value from Attribute'
+node.default['test-cookbook']['attribute1'] = 'its sucessfull'
