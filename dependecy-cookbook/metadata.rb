b5979ae611320714564ced87a649b3a0391f080c
Biswajit Das <biswajitbangalore@gmail.com>
2017-01-19 13:55:08 +0530

    created metadata

---
+++ dependecy-cookbook/metadata.rb
@@ -0,0 +1,11 @@
+name 'dependency-cookbook'
+maintainer 'Biswajit Das'
+maintainer_email 'biswajitbangalore@gmail.com'
+license 'All rights reserved'
+description 'This cookbook is to test dependency'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version '1.0.0'
+%w(redhat suse).each do |os|
+  supports os
+end
+dependes 'test-cookbook'
